5c7cd237f1dcb070694203e2dbebf20d4bb8245e
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-03-31 15:04:02 +0200

    solver

--- graphSolver.py
+++ graphSolver.py
@@ -13,9 +13,19 @@ import datetime
 import pickle
 import time
 
+
+IDENTICAL = 0
+CONV_IDENTICAL = 0
+DIFFERENT = 1
+
+WEIGHT_EXISTING_ATTACKING_EDGES = -5
+WEIGHT_EXISTING_EQUIVALENT_EDGES = 20
+WEIGHT_ADDITIONAL_ATTACKING_EDGES = -5
+WIGHT_NORMAL_EDGES = 10
+
 class GraphSolver():
 
-    def __init__(self, additional_edges = 200):
+    def __init__(self):
         self.G = MyGraph()
         self.H = MyGraph()
 
@@ -33,46 +43,94 @@ class GraphSolver():
         self.term2id = {}
         self.id2term = {}
         self.id2encode = {}
-        self.additional_edges = additional_edges
+        self.existing_equivalent_edges = []
+        self.existing_attacking_edges = [] # already in the graph
+        self.additional_attacking_edges = [] # all additional edges in the graph
 
-    def choose_nodes(self):
-        # g = self.G.subgraphs[0]
-        # i1 = random.choice(list(g.nodes))
-        # i2 = random.choice(list(g.nodes))
-        # while i1 == i2:
-        #     i2 = random.choice(list(g.nodes))
-        # return (i1, i2)
-        while True:
-            k = random.choice(list(self.domain_subdomain.keys()))
-            if len(self.domain_subdomain[k]) >= 2:
-                (t1, t2) = random.sample(self.domain_subdomain[k], 2)
-                if (len (nx.shortest_path(self.G.subgraphs[0], source = t1, target = t2)) > 4):
-                    return (t1, t2)
-
-    def compute_weight(self, t1, t2): # the most important function for now
-        weight = 0
-        if (t1, t2) in self.G.subgraphs[0].edges:
-            weight = 10
-        else:
-            t1_name = t1.rsplit('/', 1)[-1]
-            t2_name = t2.rsplit('/', 1)[-1]
-
-
-            if t1_name != '' and t2_name != '':
-                if urllib.parse.quote(t2_name) == t1_name or t2_name == urllib.parse.quote(t1_name):
-                    print ('\n\nt1 = ', t1_name)
-                    print ('t1 also = ', urllib.parse.quote(t1_name))
-                    print ('t2 = ', t2_name)
-                    print ('t2 also = ', urllib.parse.quote(t2_name))
-                    print ('SAME : ',t1, t2)
-                    print ('SAME : ',t1_name, t2_name)
-                    weight = 20
+    def compare_names (self, t1, t2):
+        n1 = t1.rsplit('/', 1)[-1]
+        n2 = t2.rsplit('/', 1)[-1]
+        if (urllib.parse.quote(n2) == n1 or n2 == urllib.parse.quote(n1)):
+            return IDENTICAL
+        else: # process it bit by bit and obtain the
+            coll_n1 = ''
+            for t in n1:
+                if t == '(' or t == ')':
+                    coll_n1 += t
                 else:
-                    weight = -6
-            else:
-                weight = 0
+                    coll_n1 += urllib.parse.quote(t)
 
-        return weight
+            coll_n2 = ''
+            for t in n2:
+                if t == '(' or t == ')':
+                    coll_n2 += t
+                else:
+                    coll_n2 += urllib.parse.quote(t)
+
+            if (n1 == coll_n2 or coll_n1 == n2):
+                return CONV_IDENTICAL # identical after conversion
+
+            # ====== NOW AGAIN ======
+            coll_n1 = ''
+            for t in n1:
+                if t == '(' or t == ')' or t == '\'':
+                    coll_n1 += t
+                else:
+                    coll_n1 += urllib.parse.quote(t)
+
+            coll_n2 = ''
+            for t in n2:
+                if t == '(' or t == ')'or t == '\'':
+                    coll_n2 += t
+                else:
+                    coll_n2 += urllib.parse.quote(t)
+            if (n1 == coll_n2 or coll_n1 == n2):
+                return CONV_IDENTICAL # identical after conversion
+
+            else:
+                # print (t1,' => ', n1, ' is now ',coll_n1)
+                # print (t2,' => ', n2, ' is now ',coll_n2,'\n')
+                return DIFFERENT
+
+    def find_existing_attacking_edges(self):
+        count_SAME = 0
+        count_DIFF = 0
+        coll_existing_attacking_edges = []
+        for (t1, t2) in self.G.subgraphs[0].edges:
+            t1_domain = tldextract.extract(t1).domain
+            t1_subdomain = tldextract.extract(t1).subdomain
+            t2_domain = tldextract.extract(t2).domain
+            t2_subdomain = tldextract.extract(t2).subdomain
+
+
+            if t1_subdomain != '' and t2_subdomain != '' and t1_domain == t2_domain and t1_subdomain == t2_subdomain:
+                if (self.compare_names(t1, t2) == DIFFERENT):
+                    self.existing_attacking_edges.append((t1, t2))
+                    count_DIFF += 1
+                    # print ('DIFF: ', t1, t2)
+                else:
+                    count_SAME += 1
+                    self.existing_equivalent_edges.append((t1, t2))
+        # print ('SAME = ', count_SAME)
+        # print ('DIFF = ', count_DIFF)
+
+    def find_additional_attacking_edges(self):
+        for x in self.domain_subdomain.keys():
+            if len(self.domain_subdomain[x]) >= 2:
+                for t1 in self.domain_subdomain[x]:
+                    for t2 in self.domain_subdomain[x]:
+                        if t1 != t2:
+                            if (self.compare_names(t1, t2) == DIFFERENT):
+                                self.additional_attacking_edges.append((t1, t2))
+
+
+    # def compute_weight(self, t1, t2): # the most important function for now
+    #     weight = 0
+    #     if (t1, t2) in self.G.subgraphs[0].edges:
+    #         weight = 10
+    #     else:
+    #         weight = -6
+    #     return weight
 
     def load_graph(self, file_name):
         self.G.load_graph(file_name)
@@ -115,24 +173,35 @@ class GraphSolver():
             self.o.add(self.id2encode[id] >= 0) # we fix all values to non-negative values
             self.o.add(self.id2encode[id] < 50) # we fix all values to non-negative values
             id += 1
-
-        for (t1, t2) in g.edges:
-            w = self.compute_weight(t1, t2)
-            # print (t1, t2, ' have weights: ', w)
-            self.o.add_soft(self.id2encode[self.term2id[t1]] == self.id2encode[self.term2id[t2]], w) # each edge within graphs
-
         # First, do a preprocessing before choosing nodes
         self.preprocessing_before_encode()
 
+        # find existing attacking edges: #TODO change the weight function
+        print ('There are in total ', len (self.G.subgraphs[0].edges))
+        edges = list(g.edges).copy()
+        self.find_existing_attacking_edges()
+        for (t1, t2) in self.existing_attacking_edges:
+            self.o.add_soft(self.id2encode[self.term2id[t1]] == self.id2encode[self.term2id[t2]], WEIGHT_EXISTING_ATTACKING_EDGES)
+            # print('existing attacking edge: ', t1, t2)
+        print('\tThere are in total: ', len (self.existing_attacking_edges), ' existing attacking edges!')
+        for (t1, t2) in self.existing_equivalent_edges:
+            self.o.add_soft(self.id2encode[self.term2id[t1]] == self.id2encode[self.term2id[t2]], WEIGHT_EXISTING_EQUIVALENT_EDGES)
+            # print('existing equivalent edge: ', t1, t2)
+        print('\tThere are in total: ', len (self.existing_equivalent_edges), ' existing equivalence edges!')
 
-        # add the edges between groups, with negative weights
-        i = 0
-        while i < int(self.additional_edges):
-            (t1, t2) = self.choose_nodes()
-            w = self.compute_weight(t1, t2)
-            # print (w, ' : ', t1, t2)
-            self.o.add_soft(self.id2encode[self.term2id[t1]] == self.id2encode[self.term2id[t2]], w)
-            i += 1
+        edges = list(filter(lambda x: x not in self.existing_attacking_edges, edges))
+        edges = list(filter(lambda x: x not in self.existing_equivalent_edges, edges))
+        print ('Now there are normal', len(edges), ' edges left')
+        # other normal edges
+        for (t1, t2) in edges:
+            self.o.add_soft(self.id2encode[self.term2id[t1]] == self.id2encode[self.term2id[t2]], WIGHT_NORMAL_EDGES) # each edge within graphs
+
+        # find additional attacking edges:
+        self.find_additional_attacking_edges()
+        for (t1, t2) in self.additional_attacking_edges:
+            self.o.add_soft(self.id2encode[self.term2id[t1]] == self.id2encode[self.term2id[t2]], WEIGHT_ADDITIONAL_ATTACKING_EDGES) # each edge within graphs
+        print('There are in total: ', len (self.additional_attacking_edges), ' additional attacking edges!')
+
 
     def solve(self):
         result = self.o.check()
@@ -147,7 +216,7 @@ class GraphSolver():
             if group_size < int(self.model.evaluate(self.id2encode[id]).as_long()):
                 group_size = int(self.model.evaluate(self.id2encode[id]).as_long())
         group_size += 1
-        print ('there are in total ', group_size, ' graphs')
+        # print ('there are in total ', group_size, ' graphs')
         for m in range (group_size):
             h = nx.Graph()
             self.H.subgraphs[m] = h
@@ -170,8 +239,18 @@ class GraphSolver():
 
                 if int(self.model.evaluate(self.id2encode[id1]).as_long()) == int(self.model.evaluate(self.id2encode[id2]).as_long()):
                     self.H.subgraphs[m].add_edge(t1, t2)
-                # and finally add h to self.H
-                # self.H.subgraphs[m] = h
+        # TODO: tidy up the group index/id so there is no empty graph in it
+        ind = 0
+        dict = {}
+        for k in self.H.subgraphs.keys():
+            g = self.H.subgraphs[k]
+            if len (g.edges) != 0:
+                dict[ind] = g
+                ind += 1
+        self.H.subgraphs = dict
+        print('there are in total ', ind + 1, ' subgraphs in the solution')
+
+
 
 
 
@@ -180,11 +259,12 @@ if __name__ == "__main__":
     start = time.time()
 
     solver = GraphSolver ()
-    solver.load_graph('./generate_data/SA2_4.csv')
+    solver.load_graph('./generate_data/SA2_8.csv')
 
     pos, labels = solver.G.save_graph(file_name = 'before.png')
     solver.encode()
     print ('now solve')
+
     solver.solve()
     print ('now decode')
     solver.decode()

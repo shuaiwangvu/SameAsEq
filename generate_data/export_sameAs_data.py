12041c2e525c23ed3fb96e3d2494bb0f609d9e20
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-05-09 21:15:04 +0200

    SB

--- generate_data/export_sameAs_data.py
+++ generate_data/export_sameAs_data.py
@@ -25,6 +25,46 @@ sameAs_dic = {}
 # path = '/Users/sw-works/Documents/backbone/sameAs_data/id2terms_0-99.csv'
 path = './closure_all/id2terms_all.csv'
 
+
+
+
+
+def obtain_graph(list_terms):
+    g = nx.Graph()
+    # add these nodes in it
+    g.add_nodes_from(list_terms)
+    for n in list_terms:
+        (triples, cardi) = hdt_file.search_triples(n, sameas, "")
+        for (_,_,o) in triples:
+            if o in list_terms:
+                g.add_edge(n, o)
+        (triples, cardi) = hdt_file.search_triples("", sameas, n)
+        for (s,_,_) in triples:
+            if s in list_terms:
+                g.add_edge(s, n)
+    return g
+
+
+
+def export_graph_csv (file_name, graph):
+    file =  open(file_name, 'w', newline='')
+    writer = csv.writer(file)
+    writer.writerow([ "SUBJECT", "OBJECT"])
+    for (l, r) in graph.edges:
+        writer.writerow([l, r])
+
+def read_graph_csv(file_name):
+    g = nx.Graph()
+    eq_file = open(file_name, 'r')
+    reader = csv.DictReader(eq_file)
+    for row in reader:
+        s = row["SUBJECT"]
+        o = row["OBJECT"]
+        g.add_edge(s, o)
+    return g
+
+
+
 with open(path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=' ')
     count = 0
@@ -105,24 +145,37 @@ print('max = ', max)
 sample_size = 10
 # Generate the VM group
 # sameAs_dic
-for V in range (10): # 0 - 99999, 10 groups
-    collect_data_VM = []
-    for k in sameAs_dic.keys():
-        terms = sameAs_dic[k] # k = group_id
-        VM_id = int(len(terms) / 10000) # an VM id
-        if VM_id == V:
-            collect_data_VM.append((k,sameAs_dic[k]))
-    # select 100 randomly from them # size
-    print (V, ' : size = ', len (collect_data_VM))
-    if len (collect_data_VM) < sample_size:
-        sample_size = len (collect_data_VM)
-    Vsample = random.sample(collect_data_VM, sample_size)
-    # export these 100 to a file
-    file_name = 'SB' + str(V) + '.csv'
-    file =  open(file_name, 'w', newline='')
-    writer = csv.writer(file)
-    # writer.writerow([ "GROUP_ID", "TERMS"])
-    for (k, terms) in Vsample:
-        writer.writerow(terms)
+# for V in range (10): # 0 - 99999, 10 groups
+collect_data_VM = []
+count = 0
+for k in sameAs_dic.keys():
+    terms = sameAs_dic[k] # k = group_id
+    size = len(terms)
+    if size > 1000:
+        export_filename = 'SB' + str(k) + '_' + str(size) +'.csv'
+        # file =  open(file_name, 'w', newline='')
+        # writer = csv.writer(file)
+        # writer.writerow([ "GROUP_ID", "TERMS"])
+        # for (k, terms) in Vsample:
+        # writer.writerow(terms)
+        g = obtain_graph(terms)
+        # export the graph
+        # nx.write_edgelist(g, export_filename)
+        export_graph_csv(export_filename, g)
+        # index += 1
 
-    print ('finished exporting for ', V)
+        # h = nx.read_edgelist(export_filename)
+        h = read_graph_csv(export_filename)
+
+        # print ('G- No. Nodes: ', len(g.nodes))
+        # print ('G- No. Edges: ', len(g.edges))
+        # print ('H- No. Nodes: ', len(h.nodes))
+        # print ('H- No. Edges: ', len(h.edges))
+        if len(h.edges) != len(g.edges):
+            print ('********* error *************')
+
+
+
+
+    count += 1
+    print ('finished exporting for ', count)
